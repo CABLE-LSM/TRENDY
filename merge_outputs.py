02364857a626a8b84f5764c48565983fa4a733dc
Whyborn <lachlan.s.whyborn@gmail.com>
2025-04-07 23:31:02 +1000

    Script appears to be in working state

--- merge_outputs.py
+++ merge_outputs.py
@@ -1,3 +1,5 @@
+import argparse
+import numpy
 import xarray
 
 def _parse_args():
@@ -18,7 +20,8 @@ def _parse_args():
     parser.add_argument(
             "-n",
             "--nruns",
-            help="Number of pseudo-parallel runs used"
+            help="Number of pseudo-parallel runs used",
+            type=int
             )
 
     parser.add_argument(
@@ -35,13 +38,45 @@ def _parse_args():
 
     return parser.parse_args()
 
+def insert_data(GlobalDataset, RunOutput, RunMask):
+    """Take the output from RunOutput and place it into the relevant global
+    indices as defined by the mask."""
+
+    print(f"Trying to open {RunOutput} and {RunMask}")
+    RunOutput = xarray.open_dataset(
+            RunOutput,
+            chunks=None,
+            cache=True
+            )
+    RunMask = xarray.open_dataset(RunMask)
+
+    # Quick check to make sure the data is valid
+    assert RunOutput["time"].size == GlobalDataset["time"].size,\
+            "Something went wrong- time dimensions are different between " +\
+            "run output and global output."
+
+    # Set up the actual mask as an array
+    Mask = ~numpy.isnan(RunMask["land"].data)
+    # Extract the indices belonging to this run
+    RunInds = [Ind for Ind, Val in numpy.ndenumerate(Mask) if Val]
+
+    # Fill in the variables
+    for Var in RunOutput.data_vars:
+        if Var not in ["latitude", "longitude"]:
+            # Set the non-space dimensions to global slices
+            LeadingDims = tuple((slice(None)
+                        for _ in range(len(RunOutput[Var].dims)-1)))
+            for (N, Ind) in enumerate(RunInds):
+                GlobalDataset[Var][LeadingDims + Ind] = \
+                         RunOutput[Var][LeadingDims + (N,)]
+
 def merge_outputs(Experiment, NRuns, YearPeriod, File):
     """Merge the outputs from each of the pseudo-parallel runs into a single
     file."""
 
     # Start by generating the file path templates
-    Landmask = lambda n: "{Experiment}/run{n}/landmask/landmask{n}.nc"
-    Output = lambda n: "{Experiment}/run{n}/outputs/{File}_{YearPeriod}.nc"
+    Landmask = lambda n: f"{Experiment}/run{n}/landmask/landmask{n}.nc"
+    Output = lambda n: f"{Experiment}/run{n}/outputs/{File}_{YearPeriod}.nc"
 
     # Get the first landmask to use as reference for grid
     RefLandmask = xarray.open_dataset(Landmask(1))
@@ -49,7 +84,7 @@ def merge_outputs(Experiment, NRuns, YearPeriod, File):
     GlobalLatitudes = RefLandmask["latitude"]
 
     # Choose which variables to exclude from the output
-    ExcludeVars = ["longitude", "latitude", "local_lon", "local_lat", "x", "y"]
+    ExcludeVars = ["x", "y", "latitude", "longitude"]
 
     # Get the first output file to use as a reference to the variables
     RefOutput = xarray.open_dataset(Output(1))
@@ -64,19 +99,28 @@ def merge_outputs(Experiment, NRuns, YearPeriod, File):
         # all dimensions except the last
         VarDims = RefOutput[Var].dims[:-1] + ("y", "x")
         DimSizes = tuple([RefOutput[Dim].size for Dim in VarDims])
-        Arr = numpy.array(DimSizes, dtype=RefOutput[Var].encoding['dtype'])
+        Arr = numpy.ndarray(DimSizes, dtype=RefOutput[Var].encoding['dtype'])
         DataVarMapping[Var] = (VarDims, Arr, RefOutput[Var].attrs)
     
     # Also set up the coords argument, which has similar requirements
-    # xarray doesn't allow coordinates with values, so just range the ones
-    # which don't exist in CABLE
+    # Exclude land, since it's only for the vector form
     CoordMapping = {}
-    Coords = [Coord for Coord in RefOutput.dims if Coord /= "land"]
-    for Coord in Coords:
+    Coords = {Coord: RefOutput[Coord]
+              for Coord in RefOutput.dims if Coord != "land"}
         
     # Set up the Dataset to write to
     WriteDataset = xarray.Dataset(
+            data_vars=DataVarMapping,
+            coords=CoordMapping
+            )
 
+    # Now retrieve and fill the data
+    for Run in range(1, NRuns+1):
+        insert_data(WriteDataset, Output(Run), Landmask(Run))
 
+    WriteDataset.to_netcdf(f"{File}_{YearPeriod}.nc")
 
+if __name__ == "__main__":
+    args = _parse_args()
 
+    merge_outputs(args.experiment, args.nruns, args.time, args.file)
